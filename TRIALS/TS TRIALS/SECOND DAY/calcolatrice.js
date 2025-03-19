18a69dee39648c01ad0ad14bb7e8e6012a8309c0
pepperagosta7 <giusepperagosta29@gmail.com>
2025-03-19 10:58:56 +0100

    concept of class added

--- TRIALS/TS TRIALS/SECOND DAY/calcolatrice.js
+++ TRIALS/TS TRIALS/SECOND DAY/calcolatrice.js
@@ -1,52 +1,59 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var readline = require("readline");
-function calcola(a, b, operazione) {
-    if (operazione === "+") {
-        return a + b;
+var Calcolatrice = /** @class */ (function () {
+    function Calcolatrice() {
     }
-    else if (operazione === "-") {
-        return a - b;
-    }
-    else if (operazione === "*") {
-        return a * b;
-    }
-    else if (operazione === "/") {
-        if (b === 0) {
-            return "Impossibile dividere per 0";
+    Calcolatrice.prototype.calcola = function (a, b, operazione) {
+        switch (operazione) {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "*":
+                return a * b;
+            case "/":
+                if (b === 0) {
+                    return "Errore: divisione per zero";
+                }
+                return a / b;
+            default:
+                return "Errore: operazione non supportata";
         }
-        return a / b;
-    }
-    else {
-        return "Operazione non valida";
-    }
-}
-/*
-const a: number = 10;
-const b: number = 5;
-const operazione: string = "+";
-console.log(calcola(a, b, operazione));
-const operazione2: string = "-";
-console.log(calcola(a, b, operazione2));
-const operazione3: string = "*";
-console.log(calcola(a, b, operazione3));
-const operazione4: string = "/";
-console.log(calcola(a, b, operazione4));
-const operazione5: string = "ciao";
-console.log(calcola(a, b, operazione5));
-*/
-var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-rl.question('Inserisci il primo numero (a): ', function (inputA) {
-    var a = parseFloat(inputA);
-    rl.question('Inserisci il secondo numero (b): ', function (inputB) {
-        var b = parseFloat(inputB);
-        rl.question('Inserisci l\'operazione (+, -, *, /): ', function (operazione) {
-            var risultato = calcola(a, b, operazione);
-            console.log("Risultato: ".concat(risultato));
-            rl.close();
+    };
+    Calcolatrice.prototype.avvia = function () {
+        var _this = this;
+        /*
+            const a: number = 10;
+            const b: number = 5;
+            const operazione: string = "+";
+            console.log(this.calcola(a, b, operazione));
+            const operazione2: string = "-";
+            console.log(this.calcola(a, b, operazione2));
+            const operazione3: string = "*";
+            console.log(this.calcola(a, b, operazione3));
+            const operazione4: string = "/";
+            console.log(this.calcola(a, b, operazione4));
+            const operazione5: string = "ciao";
+            console.log(this.calcola(a, b, operazione5));
+        */
+        var rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
         });
-    });
-});
+        rl.question("Inserisci il primo numero (a): ", function (inputA) {
+            var a = parseFloat(inputA);
+            rl.question("Inserisci il secondo numero (b): ", function (inputB) {
+                var b = parseFloat(inputB);
+                rl.question("Inserisci l'operazione (+, -, *, /): ", function (operazione) {
+                    var risultato = _this.calcola(a, b, operazione);
+                    console.log("Risultato: ".concat(risultato));
+                    rl.close();
+                });
+            });
+        });
+    };
+    return Calcolatrice;
+}());
+var calcolatrice = new Calcolatrice();
+calcolatrice.avvia();
--- TRIALS/TS TRIALS/SECOND DAY/calcolatrice.ts
+++ TRIALS/TS TRIALS/SECOND DAY/calcolatrice.ts
@@ -1,52 +1,60 @@
-import * as readline from 'readline';
+import * as readline from "readline";
 
-function calcola(a: number, b: number, operazione: string): number | string {
-    if (operazione === "+"){
+class Calcolatrice {
+  public calcola(a: number, b: number, operazione: string): number | string {
+    switch (operazione) {
+      case "+":
         return a + b;
-    } else if (operazione === "-"){
+      case "-":
         return a - b;
-    } else if (operazione === "*"){
+      case "*":
         return a * b;
-    } else if (operazione === "/"){
-        if(b === 0){
-            return "Impossibile dividere per 0";
+      case "/":
+        if (b === 0) {
+          return "Errore: divisione per zero";
         }
         return a / b;
-    } else {
-        return "Operazione non valida";
+      default:
+        return "Errore: operazione non supportata";
     }
-}
-/*
-const a: number = 10;
-const b: number = 5;
-const operazione: string = "+";
-console.log(calcola(a, b, operazione));
-const operazione2: string = "-";
-console.log(calcola(a, b, operazione2));
-const operazione3: string = "*";
-console.log(calcola(a, b, operazione3));
-const operazione4: string = "/";
-console.log(calcola(a, b, operazione4));
-const operazione5: string = "ciao";
-console.log(calcola(a, b, operazione5));
+  }
+  
+  public avvia(): void {
+
+/*    
+    const a: number = 10;
+    const b: number = 5;
+    const operazione: string = "+";
+    console.log(this.calcola(a, b, operazione));
+    const operazione2: string = "-";
+    console.log(this.calcola(a, b, operazione2));
+    const operazione3: string = "*";
+    console.log(this.calcola(a, b, operazione3));
+    const operazione4: string = "/";
+    console.log(this.calcola(a, b, operazione4));
+    const operazione5: string = "ciao";
+    console.log(this.calcola(a, b, operazione5));
 */
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+    rl.question("Inserisci il primo numero (a): ", (inputA) => {
+      const a = parseFloat(inputA);
 
-rl.question('Inserisci il primo numero (a): ', (inputA) => {
-    const a = parseFloat(inputA);
-
-    rl.question('Inserisci il secondo numero (b): ', (inputB) => {
+      rl.question("Inserisci il secondo numero (b): ", (inputB) => {
         const b = parseFloat(inputB);
 
-        rl.question('Inserisci l\'operazione (+, -, *, /): ', (operazione) => {
-            const risultato = calcola(a, b, operazione);
-            console.log(`Risultato: ${risultato}`);
-            rl.close();
+        rl.question("Inserisci l'operazione (+, -, *, /): ", (operazione) => {
+          const risultato = this.calcola(a, b, operazione);
+          console.log(`Risultato: ${risultato}`);
+          rl.close();
         });
+      });
     });
-});
+  }
+}
 
+const calcolatrice = new Calcolatrice();
+calcolatrice.avvia();
